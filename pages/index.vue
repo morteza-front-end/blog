4826cdf7a604fdb67648164f3cb746c338d64729
Morteza Hosseinzadeh <morteza.front.end@gmail.com>
2024-06-23 22:34:37 +0330

    linked post

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,9 @@ list.push(...data.value)
 <template>
 <h1 class="text-3xl font-bold">Blogs</h1>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-3 py-3">
-      <BlogsCard  v-for="post in list" :key="post.id" :post="post" />
+      <nuxt-link v-for="post in list" :key="post.id" :to="`/blogs/${post.id}`">
+        <BlogsCard   :post="post" />
+        </nuxt-link>
+      
     </div>
 </template>
